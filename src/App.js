edf7d8a623cf746fbad712c502fa37aa143fa3c7
david <daviddecastro42@live.co.uk>
2020-11-17 14:32:04 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='app'>
+        <Router>hello world</Router>
+      </div>
+    );
+  }
+}
+
+export default App;
