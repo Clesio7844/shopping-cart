44906ddc7cbb85d174375c3d0d215fb88cb17275
david <daviddecastro42@live.co.uk>
2020-11-17 17:12:06 +0000

    add product, section compoonet and csspage'

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
+import Products from './section/Product';
+import Details from './section/Details';
+import { Route } from 'react-router-dom';
 
 export class Section extends Component {
   render() {
-    return <div>section</div>;
+    return (
+      <section>
+        <Route path='/product' component={Products} exact />
+        <Route path='/product/:id' component={Details} />
+      </section>
+    );
   }
 }
 
---
+++ src/components/section/Details.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Details extends Component {
+  render() {
+    return <div>details</div>;
+  }
+}
+
+export default Details;
