a2106e1d1b9edf8ca0d00122461ae301d985ce94
david <daviddecastro42@live.co.uk>
2020-11-17 14:42:43 +0000

    add header, section component and css, sectionfolder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Header from './components/Header';
+import Section from './components/Section';
 
 class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <Router>hello world</Router>
+        <Router>
+          <Header />
+          <Section />
+        </Router>
       </div>
     );
   }
